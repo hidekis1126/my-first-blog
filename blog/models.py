9c8a2331a17fa721baa96d26d92f68b7e526341c
hidekis1126 <consulart.hmk@gmail.com>
2022-02-02 02:56:00 +0900

    コメントの承認と削除機能追加

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -15,9 +14,7 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
-
-        
+        return self.title    
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
@@ -33,4 +30,4 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
     
-    
+        
